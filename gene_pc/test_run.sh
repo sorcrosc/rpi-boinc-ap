29afcdaf1df9fc70b49d4275f012fd88c45df5e7
sorcrosc <lomastrolo@gmail.com>
2016-12-10 21:41:04 +0000

    gene armv8 app

--- gene_pc/test_run.sh
+++ gene_pc/test_run.sh
@@ -2,18 +2,20 @@
 
 for app in bin/pc*;do
 
-
-# clean file of old runs
-rm -f boinc_lockfile stderr.txt
-
 # input data
 #   * input/experiments.csv
 #   * input/tile.txt
 
 # results stored in
 #   * output/
-echo $app
+
+echo
+echo -\> ${app##*/}
 time $app input/tile.txt output/output.txt 0.05 1 393
 echo
 
+# clean files of run
+rm -f boinc_lockfile boinc_finish_called stderr.txt
+
 done
+
