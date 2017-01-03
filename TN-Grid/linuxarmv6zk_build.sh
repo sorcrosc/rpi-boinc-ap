05b6a127141bc29f47da4503fc1ffa4db390935a
sorcrosc <lomastrolo@gmail.com>
2017-01-04 00:11:01 +0100

    TN-Grid optimized app armv6

--- TN-Grid/linuxarmv6zk_build.sh
+++ TN-Grid/linuxarmv6zk_build.sh
@@ -2,25 +2,24 @@
 
 # This script assume that the PC++(pc-boinc) and boinc are in beside folders
 
-# Configure boinc for crosscompile
-# ./configure --host=arm-unknown-linux-gnueabihf  --disable-server --disable-manager --disable-client --enable-optimize CFLAGS='-ftree-vectorize' CXXFLAGS='-ftree-vectorize' LDFLAGS=-static-libgcc CC=armv6zk-rpi-linux-gnueabihf-gcc CXX=armv6zk-rpi-linux-gnueabihf-g++
+# Configure boinc for crosscompiling
+# ./configure --host=armv6zk-rpi-linux-gnueabihf --target=arm-unknown-linux-gnueabihf --disable-server --disable-manager --disable-client --enable-optimize CFLAGS='-ftree-vectorize' CXXFLAGS='-ftree-vectorize' LDFLAGS='-static-libgcc' CC=armv6zk-rpi-linux-gnueabihf-gcc CXX=armv6zk-rpi-linux-gnueabihf-g++
 
 echo "*****************************"
 echo "* BUILD PC++ (BOINC) armv6zk (rpi1) *"
 echo "*****************************"
 
+here=$(pwd)
 cd ../pc-boinc/src
 
 export CXX=armv6zk-rpi-linux-gnueabihf-g++
 export CC=armv6zk-rpi-linux-gnueabihf-gcc
 
-# crosstool-ng -march=armv6zk -mtune=arm1176jz-s -mfpu=vfp
-# armv6zk-rpi-linux-gnueabihf.config for this configuration. Rename as .config
-export EXECUTABLE=pc_armv6zk_vfp ARCH='-march=armv6zk -mtune=arm1176jz-s -mfpu=vfp -mfloat-abi=hard'
+# crosstool-ng .config file: armv6zk-rpi-linux-gnueabihf.config.
+export EXECUTABLE=pc_armv6zk_vfp ARCH='-fsigned-char -ftree-vectorize -march=armv6zk -mtune=arm1176jz-s -mfpu=vfp -mfloat-abi=hard'
 
-ln -sf $($CXX -print-file-name=libstdc++.a)
-make -f ../../gene_pc/Makefile_arm clean
-make -f ../../gene_pc/Makefile_arm
+make -f $here/Makefile_arm clean
+make -f $here/Makefile_arm
 
-mv ../bin/pc* ../../gene_pc/bin/
+mv ../bin/pc* $here/bin/
 cd -
